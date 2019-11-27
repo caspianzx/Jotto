bb0aa85581109ff5a31bea72a55329a9d4fb5666
caspianzx <caspianzx@gmail.com>
2019-11-27 15:35:56 +0800

    created congrats component and passed 3 tests

--- src/Congrats.js
+++ src/Congrats.js
@@ -2,7 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Congrats = props => {
-  return <div></div>;
+  if (props.success) {
+    return (
+      <div data-test='component-congrats'>
+        <span data-test='congrats-message'>
+          Congratulation! You guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test='component-congrats' />;
+  }
 };
 
 Congrats.propTypes = {};
