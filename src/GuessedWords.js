a470e0f0d23ca2dfbf6af807c2dcb89dbb1d5eaf
caspianzx <caspianzx@gmail.com>
2019-11-28 14:55:41 +0800

    bootstraped components

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -10,20 +10,21 @@ const GuessedWords = props => {
   } else {
     const guessedWordsRows = props.guessedWords.map((word, index) => (
       <tr data-test='guessed-word' key={index}>
-        <td>{word.guessedWord}</td> <td>{word.letterMatchCount}</td>
+        <td>{word.guessedWord}</td>
+        <td>{word.letterMatchCount}</td>
       </tr>
     ));
     content = (
       <div data-test='guessed-words'>
         <h3>Guessed Words</h3>
-        <table>
-          <thead>
+        <table className='table table-sm'>
+          <thead className='thead-light'>
             <tr>
               <th>Guess</th>
               <th>Matching Letters</th>
             </tr>
           </thead>
-          <tbody> {guessedWordsRows}</tbody>
+          <tbody>{guessedWordsRows}</tbody>
         </table>
       </div>
     );
