19a5aa0d90fe5ce61fae42ea0390e09edbb48cbb
caspianzx <caspianzx@gmail.com>
2019-11-28 21:59:19 +0800

    wrote integration test for redux thunk

--- src/integration.test.js
+++ src/integration.test.js
@@ -2,11 +2,30 @@ import { storeFactory } from '../test/testUtils';
 import { guessWord } from './actions';
 
 describe('guessWord action dispatcher', () => {
+  const secretWord = 'party';
+  const unsuccessfulGuess = 'train';
+
   describe('no guessed words', () => {
-    test('update state correctly for unsuccessful guess', () => {});
+    let store;
+    const initialState = { secretWord };
+    beforeEach(() => {
+      store = storeFactory(initialState);
+    });
+    test('update state correctly for unsuccessful guess', () => {
+      store.dispatch(guessWord(unsuccessfulGuess));
+      //getState() functions returns and object
+      const newState = store.getState();
+      const expectedState = {
+        ...initialState,
+        success: false,
+        guessedWords: [{ guessedword: unsuccessfulGuess, letterMatchCount: 3 }]
+      };
+      //toEqual is used with object as it is mutable
+      expect(newState).toEqual(expectedState);
+    });
     test('update state correctly for successful guess', () => {});
   });
-  describe('no guessed words', () => {
+  describe('some guessed words', () => {
     test('update state correctly for unsuccessful guess', () => {});
     test('update state correctly for successful guess', () => {});
   });
