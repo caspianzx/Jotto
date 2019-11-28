2bc248749a69917bf32ee7f557bc4910e37b4b4d
caspianzx <caspianzx@gmail.com>
2019-11-28 21:16:22 +0800

    installed redux thunk

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,12 +1,21 @@
 import checkPropTypes from 'check-prop-types';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import rootReducer from '../src/reducer/index';
+import { middlewares } from '../src/configureStore';
 
-//creating storeFactory
+/**
+ * Creating a testing store with imported reducers, middleware,
+ * @param {object} initialState -Initial state for store
+ * @function storeFactory
+ * @returns {Store} - redux store
+ */
 
 export const storeFactory = initialState => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(
+    createStore
+  );
+  return createStoreWithMiddleware(rootReducer, initialState);
 };
 
 //find attribute
