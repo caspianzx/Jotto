aebb527e63884d1cc2d3508a98f7a167abf9e66b
caspianzx <caspianzx@gmail.com>
2019-12-01 15:26:09 +0800

    added think for guessedWord piece of state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,31 @@
+import { getLetterMatchCount } from '../helpers/index';
+
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
   GUESS_WORD: 'GUESS_WORD'
 };
 
+/**
+ * Returns Redux Thunk function that dispatches GUESS_WORD action
+ * and conditionally CORRECT_GUESS action
+ * @function - guessWord
+ * @param {string} - guessedWord
+ * @return {function} - Redux Thunk function
+ */
+
 export const guessWord = guessedWord => {
-  return function(dispatch, getState) {};
+  return function(dispatch, getState) {
+    const secretWord = getState().secretWord;
+    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
+    dispatch({
+      type: actionTypes.GUESS_WORD,
+      payload: { guessedWord, letterMatchCount }
+    });
+
+    if (guessedWord === secretWord) {
+      dispatch({
+        type: actionTypes.CORRECT_GUESS
+      });
+    }
+  };
 };
--- src/reducer/guessedWordReducer.js
+++ src/reducer/guessedWordReducer.js
@@ -1,3 +1,18 @@
-export default (state, action) => {
-  return null;
+import { actionTypes } from '../actions/index';
+
+/**
+ * @function - guessedWordReducer
+ * @param {array} state - Array of Guessed words.
+ * @param {object} action - action to be reduced.
+ * @return {array} - new guessedWords state
+ */
+
+export default (state = [], action) => {
+  //note: action argument here is different from actionTypes
+  switch (action.type) {
+    case actionTypes.GUESS_WORD:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
 };
